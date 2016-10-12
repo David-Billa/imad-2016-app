5ccb810bfa5d9804fa56dbe0ab1fcf11d7d4f36d
David-Billa <masp203@gmail.com>
2016-10-12 23:42:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
-element.innerHTML='NEW VALUE'
+element.innerHTML='NEW VALUE';
+var img = document.getElementById('madi');
+madi.onclick = function()
+{
+    madi.style.marginleft = "100px";
+};
