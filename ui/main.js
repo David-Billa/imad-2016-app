e3a8fa7f78d4eebaa80de7841ec22f6a73971447
David Billa <masp203@gmail.com>
2016-10-17 23:34:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML='NEW VALUE';
-var img = document.getElementById('madi');
-
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+10;
-    img.style.marginbottom=marginLeft+'100px';
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function()
-{
- setInterval(moveRight,100);   
-    };
+var button = document.getElementById("counter");
+var count=0;
+button.onclick = function(){
+ count = count+1;
+ var span = document.getElementById("count");
+ count.innerHTML = count.toString();
+    
+};
